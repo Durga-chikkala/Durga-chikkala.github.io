03ed34e6f682db7611b68c3c096f2931cc1ad34b
kamalatha99 <chdurga2001@gmail.com>
2021-06-26 09:18:50 +0530

    l

---
+++ intern/script.js
@@ -0,0 +1,24 @@
+function mOver(obj)
+{
+    document.querySelector(".create").style.display="flex";
+
+}
+function mOut(obj)
+{
+    document.querySelector(".create").style.display="none";
+
+}
+function mOver1(obj)
+{
+    document.querySelector(".cat").style.display="flex";
+
+}
+function mOut1(obj)
+{
+    document.querySelector(".cat").style.display="none";
+
+}
+
+
+
+

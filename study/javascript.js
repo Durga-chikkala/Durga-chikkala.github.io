a87e66252e7d9c15b914b87082e61b737ff567dc
kamalatha99 <chdurga2001@gmail.com>
2021-08-08 19:44:58 +0530

    app

---
+++ study/javascript.js
@@ -0,0 +1,20 @@
+function profile()
+{
+    location.href="profile.html";
+}
+function ai()
+{
+    location.href="Ai.html";
+}
+function target()
+{
+    location.href="target.html";
+}
+function home()
+{
+    location.href="index.html";
+}
+function Dashboard()
+{
+    location.href="dashboard.html";
+}
